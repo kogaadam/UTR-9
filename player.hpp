2fa4dc5cc921f85e6a5228fc12497166ab93ca50
Adam Kogan <43ada21@gmail.com>
2018-03-10 17:06:48 -0800

    removed unnecessary comments

--- player.hpp
+++ player.hpp
@@ -7,15 +7,7 @@
 using namespace std;
 
 //Table of weights representing how optimal the position is
-extern int scoreTable[8][8]; /*[8][8] = {{7, 2, 5, 4, 4, 5, 2, 7},
-					 {2, 1, 3, 3, 3, 3, 1, 2},
-				  	 {5, 3, 6, 5, 5, 6, 3, 5},
-				     {4, 3, 5, 6, 6, 5, 3, 4},
-				     {4, 3, 5, 6, 6, 5, 3, 4},
-				     {5, 3, 6, 5, 5, 6, 3, 5},
-				     {2, 1, 3, 3, 3, 3, 1, 2},
-				     {7, 2, 5, 4, 4, 5, 2, 7},
-				    };*/
+extern int scoreTable[8][8];
 
 class Player {
 
